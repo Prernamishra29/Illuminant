ff77ef1871839429fc83e9935d624308e88060b1
Prerna Mishra <124295405+Prernamishra29@users.noreply.github.com>
2025-05-23 09:32:14 +0530

    Contact Updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,9 @@ const app = express();
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:5500",
-  "http://127.0.0.1:5500/docs" // Optional, include if you're serving from here
+  "http://127.0.0.1:5500/docs" ,
+  "https://prernamishra29.github.io/Illuminant/index.html"
+  // Optional, include if you're serving from here
 ];
 
 // CORS Middleware
